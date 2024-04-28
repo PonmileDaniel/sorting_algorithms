10712aad000531719233d1520e8d4649bf4844a2
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-04-28 08:30:12 -0700

    quick

---
+++ 3-quick_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * partitin - Partitions the arrays and return the index
+ * @array: Pointer to the first element of the array'
+ * @low: Starting index of the partition to be sorted
+ * @high: Ending index of the partition
+ * @size: Number of element in the array
+ * Return: Index of the pivot element
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot, i, j, tmp;
+
+	pivot = array[high];
+	i = low - 1;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+	tmp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = tmp;
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * quicksort - Recursively sorts the array
+ * @array: Pointer to the first element of the array
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Number of element
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quicksort(array, low, p - 1, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sort an array of integers 
+ * @array: Pointer to the first element of theh array
+ * @size: Number of element
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
